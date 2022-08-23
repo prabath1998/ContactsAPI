1dc104a233980e51cbc0b308cc4bbc8c8f2e35bb
prabath1998 <prabhathudayanga1998@gmail.com>
2022-08-23 23:39:16 +0530

    get all contacts

---
+++ ContactsAPI/Controllers/ContactsController.cs
@@ -0,0 +1,23 @@
+﻿using ContactsAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ContactsApiDbContext dbContext;
+
+        public ContactsController(ContactsApiDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetContacts() {
+            return Ok(dbContext.Contacts.ToList());
+        }
+    }
+}
